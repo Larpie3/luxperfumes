5eab27e3cea294d481678d6483d75a62a46463d4
Ralph Castañares <ralphcstnrs3@gmail.com>
2026-02-13 17:14:14 +0800

    Implement custom cursor on mouse move for desktop
    Add custom cursor functionality for larger screens.

--- script.js
+++ script.js
@@ -169,3 +169,14 @@ function setupFilters(products){
     });
   });
         } 
+
+if (window.innerWidth >= 1024) {
+  const cursor = document.createElement("div");
+  cursor.classList.add("custom-cursor");
+  document.body.appendChild(cursor);
+
+  document.addEventListener("mousemove", e => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+  });
+}
